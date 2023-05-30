df133fb45e37c19ce6c2f6aef5332a1285759460
Aachman Mittal <aachman.mittal@essentia.dev>
2023-05-30 22:04:45 +0530

    part 3: Views and Templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world. You are at the polls index page.")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_questions": latest_questions
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
-# Create your views here.
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question with id %s does not exist", question_id)
+    # return render(request, "polls/detail.html", {"question": question})
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
