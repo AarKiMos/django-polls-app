f95b5b9e63e16deca9ae320f22010dc803485318
Aachman Mittal <aachman.mittal@essentia.dev>
2023-06-01 09:20:25 +0530

    Part4: avoid race condition

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -43,7 +44,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, "polls/detail.html", {"question": question, "error_message": "You didn't select a choice.", })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
